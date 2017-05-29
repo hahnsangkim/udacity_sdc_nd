b6167a5773521c1a658fdb5d9f6711b4ee107049
H Kim <fido2478@gmail.com>
2017-05-30 06:14:31 +0900

    first commit

---
+++ src/MPC.h
@@ -0,0 +1,25 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+using namespace std;
+
+class MPC {
+ public:
+ 	size_t N_;
+	double dt_;
+	double steering_angle;
+	double throttle;
+
+  MPC();
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuatotions.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+};
+
+#endif /* MPC_H */
